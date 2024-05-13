869e2622843a43767aa92be6a34a9b1ac9f47ea2
MiguelCardoso <miguel40cardoso@gmail.com>
2024-05-13 22:24:09 +0100

    Divided project into client and server; initialized backend with database connection and initial project structure

---
+++ server/src/main/java/com/project/reelRadar/ReelRadarApplication.java
@@ -0,0 +1,13 @@
+package com.project.reelRadar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReelRadarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReelRadarApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/project/reelRadar/ReelRadarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.reelRadar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReelRadarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
