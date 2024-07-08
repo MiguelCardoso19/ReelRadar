b541eb38b060aa2a5cfd237a0bfd0df838c01d1c
MiguelCardoso <miguel40cardoso@gmail.com>
2024-07-08 14:00:59 +0100

    Started Docker containerization with Dockerfile

--- server/src/main/java/com/project/reelRadar/ReelRadarApplication.java
+++ server/src/main/java/com/project/reelRadar/ReelRadarApplication.java
@@ -8,5 +8,20 @@ public class ReelRadarApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReelRadarApplication.class, args);
+
+
+		/** Testing Docker **/
+		new Thread(() -> {
+			while (true) {
+				try {
+					Thread.sleep(5000);
+					System.out.println("live");
+					System.out.println("####");
+				} catch (InterruptedException e) {
+					Thread.currentThread().interrupt();
+					System.err.println("Thread was interrupted, Failed to complete operation");
+				}
+			}
+		}).start();
 	}
 }
---
+++ server/src/test/java/com/project/reelRadar/services/UserServiceImplTest.java
@@ -0,0 +1,51 @@
+package com.project.reelRadar.services;
+
+import com.project.reelRadar.dtos.RegisterRequestDTO;
+import com.project.reelRadar.exceptions.UserAlreadyExistsException;
+import com.project.reelRadar.models.User;
+import com.project.reelRadar.repositories.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class UserServiceImplTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @InjectMocks
+    private UserServiceImpl userService;
+
+    private RegisterRequestDTO registerRequestDTO;
+
+
+    @BeforeEach
+    void setUp() {
+        registerRequestDTO = new RegisterRequestDTO("testUsername", "testPassword", "testEmail@example.com");
+    }
+
+    @Test
+    public void testSaveUserSuccessfully() throws UserAlreadyExistsException {
+
+        when(userRepository.findByUsername(registerRequestDTO.username())).thenReturn(Optional.empty());
+        when(passwordEncoder.encode(registerRequestDTO.password())).thenReturn("encodedPassword");
+
+        User savedUser = userService.save(registerRequestDTO);
+
+        verify(userRepository, times(1)).findByUsername(registerRequestDTO.username());
+        verify(userRepository, times(1)).save(any(User.class));
+        assertEquals("encodedPassword", savedUser.getPassword());
+        assertEquals(registerRequestDTO.username(), savedUser.getUsername());
+        assertEquals(registerRequestDTO.email(), savedUser.getEmail());
+    }
+}
